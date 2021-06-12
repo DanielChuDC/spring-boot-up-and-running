b38a9455fc3b737fce688b9e4b20418097ec6c11
danielchu <52316624+DanielChuDC@users.noreply.github.com>
2021-06-12 16:03:42 +0800

    Update code to use JPA CRUD repo to fetch and modify data

--- src/main/java/com/example/springtesting/SpringTestingApplication.java
+++ src/main/java/com/example/springtesting/SpringTestingApplication.java
@@ -2,6 +2,7 @@ package com.example.springtesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,15 +23,17 @@ public class SpringTestingApplication {
 
 }
 
+interface CoffeeRepository extends CrudRepository<Coffee, String> {}
+
 @RestController
 @RequestMapping("/coffees")
 class RestApiDemoController {
 
-    
-    private List<Coffee> coffees= new ArrayList<>();
+    private final CoffeeRepository coffeeRepository;
 
-    public RestApiDemoController() {
-        coffees.addAll(
+    public RestApiDemoController(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+        this.coffeeRepository.saveAll(
                 List.of(
                         new Coffee("Café Cereza"),
                         new Coffee("Café Ganador"),
@@ -50,51 +53,31 @@ class RestApiDemoController {
     // This method works the same for the @RequestMapping
     @GetMapping
     Iterable<Coffee> getCoffees() {
-        return coffees;
+        return coffeeRepository.findAll();
     }
 
     @GetMapping("/{id}")
     Optional<Coffee> getCoffeeById(@PathVariable String id)
     {
-        for(Coffee c: coffees){
-            if(c.getId().equals(id))
-            {
-                return Optional.of(c);
-            }
-        }
-        // Optional means if an empty Optional<Coffee> returned if the id requested isn’t present in our small group of coffees:
-        return Optional.empty();
+        return coffeeRepository.findById(id);
     }
 
     @PostMapping
     Coffee postCoffee(@RequestBody Coffee coffee) {
-        coffees.add(coffee);
-        return coffee;
+        return coffeeRepository.save(coffee);
     }
 
     @PutMapping("/{id}")
     ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-        int coffeeIndex = -1;
-
-        for (Coffee c: coffees) {
-            if (c.getId().equals(id)) {
-                coffeeIndex = coffees.indexOf(c);
-                coffees.set(coffeeIndex, coffee);
-            }
-        }
-
-        //  Instead of returning only the updated or created Coffee object,
-        //  the putCoffee method will now return a ResponseEntity containing said Coffee and
-        //  the appropriate HTTP status code: 201 (Created)
-        //  if the PUT coffee didn’t already exist, and 200 (OK) if it existed and was updated.
-        return (coffeeIndex == -1) ?
-                new ResponseEntity<>(postCoffee(coffee), HttpStatus.CREATED) :
-                new ResponseEntity<>(coffee, HttpStatus.OK);
+        return (!coffeeRepository.existsById(id))
+                ? new ResponseEntity<>(coffeeRepository.save(coffee),
+                HttpStatus.CREATED)
+                : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     void deleteCoffee(@PathVariable String id) {
-        coffees.removeIf(c -> c.getId().equals(id));
+        coffeeRepository.deleteById(id);
     }
 }
 
